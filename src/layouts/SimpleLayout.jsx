7c8b306468f809c1fdeb61fa930343242b3af979
freyler contrera <freylerconro@gmail.com>
2026-04-03 17:15:17 -0500

    docs: comenta componente SimpleLayout

--- src/layouts/SimpleLayout.jsx
+++ src/layouts/SimpleLayout.jsx
@@ -6,6 +6,8 @@ import Footer from '../components/Footer';
 export default function SimpleLayout() {
 
     return(
+         // Contenedor principal que ocupa toda la pantalla
+          //  Header altura automática, contenido flexible, footer automático
         <div className="min-h-screen grid grid-rows-[auto_1fr_auto] bg-zinc-50 overflow-hidden">
             <Header />
             <Outlet />
